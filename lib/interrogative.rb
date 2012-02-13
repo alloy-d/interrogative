4250b8acc1a6093e72803814991d143a9abae9ea
Adam Lloyd <adam@alloy-d.net>
2012-02-13 00:11:39 -0500

    Looks about right.

---
+++ lib/interrogative.rb
@@ -0,0 +1,42 @@
+require 'interrogative/question'
+
+# A mixin for curious classes.
+module Interrogative
+  # Get the array of all noted questions.
+  #
+  # @return [Array<Question>] array of all noted questions.
+  def questions; @_questions; end
+
+  # Give instructions for dealing with new questions.
+  #
+  # @param [Proc] postprocessor a block to run after adding a question;
+  #                             the question is given as the argument.
+  def when_questioned(&postprocessor)
+    (@_question_postprocessors||=[]) << postprocessor
+  end
+
+  # Give a new question.
+  #
+  # @param [Symbol, String] name the name (think <input name=...>) of
+  #   the question.
+  # @param [String] label the text of the question (think <label>).
+  # @param [Hash] attrs additional attributes for the question.
+  # @option attrs [Boolean] :long whether the question has a long answer
+  #   (think <textarea> vs <input>).
+  # @option attrs [Boolean] :multiple whether the question could have
+  #   multiple answers.
+  # @return [Question] the new Question.
+  def question(name, text, attrs={})
+    q = Question.new(name, text, self, attrs)
+    (@_questions||=[]) << q
+    
+    unless @_question_postprocessors.nil?
+      @_question_postprocessors.each do |postprocessor|
+        postprocessor.call(q)
+      end
+    end
+
+    return q
+  end
+end
+
---
+++ lib/interrogative/question.rb
@@ -0,0 +1,67 @@
+require 'json'
+require 'yaml'
+
+module Interrogative
+
+  # A question with a unique name and a textual representation.
+  #
+  # Designed to translate well into an HTML form element without
+  # betraying the fact that it's meant to transform into an HTML form
+  # element.
+  class Question
+    attr_accessor :name, :text, :attrs
+
+    # @see Interrogative#question
+    def initialize(name, text, owner=nil, attrs={})
+      @name = name or raise ArgumentError, "A question must have a name."
+      @text = text or raise ArgumentError, "A question must have a label."
+      @owner = owner
+      @attrs = attrs
+    end
+
+    # Possible answers for the question.
+    #
+    # Returns `nil` unless the class that included Interrogative
+    # responds to a method called `#{ name }_options`; otherwise, it
+    # returns the result of calling that method.
+    #
+    # Options should be either an Array or a Hash.
+    # In the case of a Hash, the format should be `{ text => value }`.
+    #
+    # @return [Array, Hash] the possible answers for the question.
+    def options
+      return nil if @owner.nil?
+
+      options_method = "#{@name}_options".intern
+      if @owner.respond_to? options_method
+        @owner.send options_method
+      end
+    end
+
+    # Returns a hash representation of the question.
+    #
+    # Attributes are merged into the top level, along with `:text` and
+    # `:name`. Possible options are nested under `:options`.
+    #
+    # @return [Hash]
+    def to_hash
+      h = @attrs.merge({
+        :text => text,
+        :name => name,
+      })
+
+      o = options
+      h[:options] = o if not o.nil?
+    end
+
+    # Returns a JSON object created from the question's hash
+    # representation.
+    #
+    # @return [String]
+    # @see #to_hash
+    def to_json
+      self.to_hash.to_json
+    end
+  end
+end
+
