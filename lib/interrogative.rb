684ddbeaa6dea6f5b2c89606314d43fa0fc77e38
Adam Lloyd <adam@alloy-d.net>
2012-05-10 14:55:11 -0400

    Better support for inheritance and instances and classes.

--- lib/interrogative.rb
+++ lib/interrogative.rb
@@ -2,41 +2,58 @@ require 'interrogative/question'
 
 # A mixin for curious classes.
 module Interrogative
-  # Get the array of all noted questions.
-  #
-  # @return [Array<Question>] array of all noted questions.
-  def questions; @_questions; end
-
-  # Give instructions for dealing with new questions.
-  #
-  # @param [Proc] postprocessor a block to run after adding a question;
-  #                             the question is given as the argument.
-  def when_questioned(&postprocessor)
-    (@_question_postprocessors||=[]) << postprocessor
-  end
-
-  # Give a new question.
-  #
-  # @param [Symbol, String] name the name (think <input name=...>) of
-  #   the question.
-  # @param [String] label the text of the question (think <label>).
-  # @param [Hash] attrs additional attributes for the question.
-  # @option attrs [Boolean] :long whether the question has a long answer
-  #   (think <textarea> vs <input>).
-  # @option attrs [Boolean] :multiple whether the question could have
-  #   multiple answers.
-  # @return [Question] the new Question.
-  def question(name, text, attrs={})
-    q = Question.new(name, text, self, attrs)
-    (@_questions||=[]) << q
-    
-    unless @_question_postprocessors.nil?
-      @_question_postprocessors.each do |postprocessor|
-        postprocessor.call(q)
+  module Methods
+    # Get the array of all noted questions.
+    #
+    # @return [Array<Question>] array of all noted questions.
+    def questions
+      questions = []
+      questions |= superclass.questions rescue []
+      questions |= self.class.questions if self.class.respond_to? :questions
+      if self.class.respond_to? :questions
+        questions |= self.class.questions
       end
+      questions | (@_questions||=[])
     end
 
-    return q
+    # Give instructions for dealing with new questions.
+    #
+    # @param [Proc] postprocessor a block to run after adding a question;
+    #                             the question is given as the argument.
+    def when_questioned(&postprocessor)
+      (@_question_postprocessors||=[]) << postprocessor
+    end
+
+    # Give a new question.
+    #
+    # @param [Symbol, String] name the name (think <input name=...>) of
+    #   the question.
+    # @param [String] label the text of the question (think <label>).
+    # @param [Hash] attrs additional attributes for the question.
+    # @option attrs [Boolean] :long whether the question has a long answer
+    #   (think <textarea> vs <input>).
+    # @option attrs [Boolean] :multiple whether the question could have
+    #   multiple answers.
+    # @return [Question] the new Question.
+    def question(name, text, attrs={})
+      q = Question.new(name, text, self, attrs)
+      (@_questions||=[]) << q
+      
+      unless @_question_postprocessors.nil?
+        @_question_postprocessors.each do |postprocessor|
+          postprocessor.call(q)
+        end
+      end
+
+      return q
+    end
+  end
+
+  def self.included(base)
+    base.extend(Interrogative::Methods)
+    base.instance_eval do
+      include Interrogative::Methods
+    end
   end
 end
 
--- test/interrogative_test.rb
+++ test/interrogative_test.rb
@@ -1,11 +1,77 @@
 require 'teststrap'
 
-context "interrogative" do
-  setup do
-    false
+class NoQuestionsTest
+  include Interrogative
+end
+
+class QuestionsTest
+  include Interrogative
+
+  question :has_questions, "Does this have questions?"
+end
+
+class SubclassWithNoQuestionsTest < QuestionsTest
+end
+
+class SubclassWithQuestionsTest < QuestionsTest
+  question :is_subclass, "Is this a subclass?"
+end
+
+context "class with no questions" do
+  setup { NoQuestionsTest }
+
+  asserts(:questions).kind_of Array
+  asserts(:questions).empty
+end
+
+context "class with one question" do
+  context "(class)" do
+    setup { QuestionsTest }
+
+    denies(:questions).empty
+    asserts(:questions).size(1)
   end
 
-  asserts "i'm a failure :(" do
-    topic
+  context "(an instance)" do
+    setup { QuestionsTest.new }
+
+    denies(:questions).empty
+    asserts(:questions).size(1)
+  end
+
+  context "(an instance with a question of its own)" do
+    setup do
+      QuestionsTest.new.tap {|q| q.question :is_instance, "Is this an instance?" }
+    end
+
+    asserts(:questions).size(2)
+  end
+end
+
+context "subclass with no questions of its own" do
+  setup { SubclassWithNoQuestionsTest }
+
+  denies(:questions).empty
+end
+
+context "subclass with a question of its own" do
+  setup { SubclassWithQuestionsTest }
+
+  denies(:questions).empty
+  asserts(:questions).size(2)
+
+  context "(instance)" do
+    setup { SubclassWithQuestionsTest.new }
+
+    denies(:questions).empty
+    asserts(:questions).size(2)
+  end
+
+  context "(instance with a question of its own)" do
+    setup do
+      SubclassWithQuestionsTest.new.tap {|i| i.question :is_instance, "Is this an instance?" }
+    end
+
+    asserts(:questions).size(3)
   end
 end
